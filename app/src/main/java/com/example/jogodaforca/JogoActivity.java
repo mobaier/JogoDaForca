c4c2dcf7025a3b3243578da2a6e30740c632cdf4
Andre Oliveira <order_yellow@hotmail.com>
2020-10-22 18:06:26 -0300

    Add title logo and improved functionalities

--- app/src/main/java/com/example/jogodaforca/JogoActivity.java
+++ app/src/main/java/com/example/jogodaforca/JogoActivity.java
@@ -19,15 +19,26 @@ public class JogoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jogo);
+
         // Pegar variáveis da outra atividade
         Bundle bundle = getIntent().getExtras();
-        TextView textView = findViewById(R.id.restante);
         assert bundle != null;
+
+        TextView textView = findViewById(R.id.restante);
+        TextView textView1 = findViewById(R.id.palavraMisteriosa);
+        TextView textView2 = findViewById(R.id.display);
+
         String restante = bundle.getString("Chances");
-        textView.setText(restante);
-        TextView textView2 = findViewById(R.id.palavraMisteriosa);
         String palavra = bundle.getString("Misterio");
-        textView2.setText(palavra);
+        // Censurar a palavra digitada
+        String incognito = "";
+        for(int i=0; i<palavra.length(); i++) {
+            incognito += "_ ";
+        }
+
+        textView.setText(restante);
+        textView1.setText(palavra);
+        textView2.setText(incognito);
     }
 
     // Adicionar as letras inseridas a uma lista de tentativas
@@ -36,13 +47,49 @@ public class JogoActivity extends AppCompatActivity {
         // Adicionar a letra a lista de tentativas
         EditText editText = findViewById(R.id.editTextLetra);
         TextView textView = findViewById(R.id.palavras);
+        TextView textView1 = findViewById(R.id.palavraMisteriosa);
+        TextView textView2 = findViewById(R.id.restante);
+        TextView textView3 = findViewById(R.id.display);
+
         String letra = editText.getText().toString();
+        String palavra = textView1.getText().toString();
+        String substituto = "";
+        String adivinhacao = "";
+        int restante = Integer.parseInt(textView2.getText().toString());
+
         arr.add(letra);
-        String adv = "";
         for(int i = 0; i < arr.size(); i++) {
-            adv += arr.get(i) + " ";
+            adivinhacao += arr.get(i) + " ";
         }
-        textView.setText(adv);
+
+        textView.setText(adivinhacao);
+
+        // Ver se a letra digitada aparece na
+        // palavra e revelar a posição da letra
+        if(palavra.contains(letra)) {
+            for(int i=0; i<palavra.length(); i++) {
+                if(letra.equals(Character.toString(palavra.charAt(i)))) {
+                    substituto += letra + " ";
+                } else {
+                    substituto += "_ ";
+                }
+            }
+
+            textView3.setText(substituto);
+
+        } else {
+            restante--;
+            textView2.setText(Integer.toString(restante));
+        }
+
+        // Se suas chances acabaram o jogo (atividade) é encerrado
+        if(restante == 0) {
+            CharSequence texto = "Fim de Jogo";
+            int duracao = Toast.LENGTH_LONG;
+            Toast toast = Toast.makeText(this, texto, duracao);
+            toast.show();
+            finish();
+        }
     }
 
     // Adicionar as palavras inseridas a uma lista de tentativas
@@ -52,24 +99,40 @@ public class JogoActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.palavras);
         TextView textView1 = findViewById(R.id.palavraMisteriosa);
         TextView textView2 = findViewById(R.id.restante);
+        TextView textView3 = findViewById(R.id.display);
+
         int restante = Integer.parseInt(textView2.getText().toString());
         String resposta = textView1.getText().toString();
         String palavra = editText.getText().toString();
-        arr.add(palavra);
-        String adv = "";
-        for(int i = 0; i < arr.size(); i++) {
-            adv += arr.get(i) + " ";
-        }
-        textView.setText(adv);
+        String adivinhacao = "";
 
+        arr.add(palavra);
+        for(int i = 0; i < arr.size(); i++) {
+            adivinhacao += arr.get(i) + " ";
+        }
+
+        textView.setText(adivinhacao);
+
+        // Ver se você acertou a palavra
         if(palavra.equals(resposta)) {
+            textView3.setText(resposta);
             CharSequence texto = "Parabéns, Você Acertou!";
             int duracao = Toast.LENGTH_LONG;
             Toast toast = Toast.makeText(this, texto, duracao);
             toast.show();
+            finish();
         } else {
             restante--;
             textView2.setText(Integer.toString(restante));
+        }
+
+        // Se suas chances acabaram o jogo (atividade) é encerrado
+        if(restante == 0) {
+            CharSequence texto = "Fim de Jogo";
+            int duracao = Toast.LENGTH_LONG;
+            Toast toast = Toast.makeText(this, texto, duracao);
+            toast.show();
+            finish();
         }
     }
 }
